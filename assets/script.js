5aa5c247ed52b304e41cd7c699095e40685922f3
Kejvin Pashaj <kevin.952011@gmail.com>
2023-08-08 11:13:51 +0100

    starter files

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const encryptedInput = document.getElementById("encrypt-input");
+const encryptButton = document.getElementById("encrypt-button");
+const decryptedOutput = document.getElementById("decrypt-output");
+
+function rot13(text){
+    
+}
