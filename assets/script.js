0656dd501e407ce24a032eb15eccc8f5b055a9ee
Kejvin Pashaj <kevin.952011@gmail.com>
2023-08-08 11:35:42 +0100

    script rot13 function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,32 @@
-const encryptedInput = document.getElementById("encrypt-input");
+const encryptedInput = document.getElementById("encrypt-input").value;
 const encryptButton = document.getElementById("encrypt-button");
 const decryptedOutput = document.getElementById("decrypt-output");
 
-function rot13(text){
-    
+function handleClick(){
+    // console.log(encryptedInput.value)
+
+    decryptedOutput.innerHTML = rot13(encryptedInput)
 }
+
+function rot13(text){
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const keyAlphabet = alphabet.substring(13) + alphabet.substring(0, 13);
+    let result = "";
+
+    for (let i = 0; i < text.length; i++){
+        let currentCharacter = text[i];
+        let index = alphabet.indexOf(currentCharacter);
+        // check for non-letter char
+        if (index !== -1){
+            let encryptedText = keyAlphabet[index]
+            result += encryptedText;
+        }
+        
+        // console.log(currentCharacter)
+    }
+    return result;
+}
+
+// rot13(encryptedInput);
+
+encryptButton.addEventListener("click", handleClick)
